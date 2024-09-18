fe437a7f92f44b7f41796ec5d872651bd0e879eb
mpha0039 <mpha0039@student.monash.edu>
2024-09-18 20:04:08 +1000

    modified prop symbols map color, shape, and size

--- week7_studio/vega_lite_vis.js
+++ week7_studio/vega_lite_vis.js
@@ -1,5 +1,3 @@
 var vg_1 = "simple_bar_chart.vega";
-// embed vg_1 in the bar_chart div
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
---
+++ week8_studio/vega_lite_vis.js
@@ -0,0 +1,3 @@
+var vg_1 = "proportional_symbols_map.vega.json";
+vegaEmbed("#prop_symbols_map", vg_1).then(function(result) {
+}).catch(console.error);
